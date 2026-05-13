105dc8188210164376b3f636f3a48f65f6190bbf
kareem.emad <kareem.emad@isoft.ae>
2026-05-13 14:06:21 +0400

    playWright project

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -19,6 +19,7 @@ public class BaseTest {
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
                         .setHeadless(false)
+                        .setSlowMo(500)
         );
 
         context = browser.newContext();
