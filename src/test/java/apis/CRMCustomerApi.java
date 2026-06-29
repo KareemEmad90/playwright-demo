dba961235fd6100f80546b3f5c7aa5525eb8c2c4
kareem.emad <kareem.emad@isoft.ae>
2026-06-29 13:27:53 +0400

    add jenkins file

--- src/test/java/apis/CRMCustomerApi.java
+++ src/test/java/apis/CRMCustomerApi.java
@@ -13,7 +13,7 @@ public class CRMCustomerApi {
 
     public Response addCustomerInfo (){
         RestAssured.useRelaxedHTTPSValidation();
-        String baseURI = "http://localhost:8201/api/customers";
+        String baseURI = "http://172.18.125.188:8201/api/customers";
 
 
         String checkEligibilityBody = AddCustomer.SubmitNewCustomerPayLoad(email);
